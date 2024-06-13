567a92ca5c419ed0797f4efa8435ba06e4ec2bb0
Edwar León <edwar.leon.m@gmail.com>
2024-06-13 00:00:18 -0500

    Fixed url api

--- src/js/bd-Api.js
+++ src/js/bd-Api.js
@@ -1,9 +1,11 @@
 const BASE_URL = "https://ecommercebackend.fundamentos-29.repl.co/";
 const BASE_LOCAL = "https://services-academlo-shopping.onrender.co/";
+const BASE_JSON = "/src/js/api-academlo.json"
+console.log(BASE_JSON)
 
 export async function getProductsApi() {
   try {
-    const data = await fetch(BASE_URL);
+    const data = await fetch(BASE_JSON);
     const res = await data.json();
 
     localStorage.setItem("products", JSON.stringify(res));
